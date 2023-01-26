db49571b89b1a59440856faf6262c018c7c65898
Shashank Parab <shashank@optibrium.com>
2023-01-26 14:59:18 +0000

    Removing unused vars

--- cerella/variables.tf
+++ cerella/variables.tf
@@ -1,24 +1,3 @@
-# variable "acm-certificate-arn" {
-#   # if empty, then we create a new certificate. Otherwise, we use the existing one
-#   default = ""
-# }
-
-# variable "cidr" {
-#   default = "10.0.0.0/16"
-#   type    = string
-# }
-
-# variable "vpc_id" {
-#   default = ""
-# }
-
-# variable "private_subnet_ids" {
-#   default = []
-# }
-
-# variable "public_subnet_ids" {
-#   default = []
-# }
 
 variable "cluster_autoscaler_version" {
   default = "v1.22.3"
@@ -37,56 +16,16 @@ variable "domain" {
   type = string
 }
 
-# # This might be overridden
-# variable "eks-instance-count" {
-#   default = 3
-# }
-
-# variable "eks-instance-type" {
-#   default = "t2.large"
-#   type    = string
-# }
-
-# variable "eks-version" {
-#   default = "1.22"
-#   type    = string
-# }
-
-# variable "hosted-zone-id" {
-#   type    = string
-#   default = "" # will disable the creation of the DNS record
-# }
-
-# # Normally this would be IP restricted,
-# variable "ingress-cidr" {
-#   type = list(string)
-# }
-
 variable "ingress_version" {
   default = "0.11.3"
 }
-
-# variable "irsa_iam_role_name" {
-#   default = "external-secrets-readonly"
-#   type    = string
-# }
-
-# variable "left-availability-zone" {
-#   default = "eu-west-1b"
-#   type    = string
-# }
-
-# variable "left-subnet-cidr" {
-#   default = "10.0.2.0/24"
-#   type    = string
-# }
 
 variable "prometheus_chart_version" {
   default = "38.0.0"
 }
 
 variable "region" {
-  type    = string
+  type = string
 }
 
 variable "registry_password" {
@@ -106,48 +45,6 @@ variable "ingest_irsa_iam_role_name" {
 variable "worker_nodes_iam_role_arn" {
   type = string
 }
-
-# variable "right-availability-zone" {
-#   default = "eu-west-1a"
-#   type    = string
-# }
-
-# variable "right-subnet-cidr" {
-#   default = "10.0.1.0/24"
-#   type    = string
-# }
-
-# variable "service-account-name" {
-#   default = "external-secrets"
-#   type    = string
-# }
-
-# variable "service-account-namespace" {
-#   default = "kube-system"
-#   type    = string
-# }
-
-# variable "ingest_service_account_namespace" {
-#   default = "blue"
-#   type    = string
-# }
-
-# variable "ingest_service_account_name" {
-#   default = "ingest"
-#   type    = string
-# }
-
-# variable "external_secret_service_account_namespace" {
-#   default = "kube-system"
-#   type    = string
-# }
-
-# variable "external_secret_service_account_name" {
-#   default = "external-secrets"
-#   type    = string
-# }
-
-# Get addon version value by running aws eks describe-addon-versions
 variable "kube_proxy_addon_version" {
   default = "v1.22.11-eksbuild.2"
   type    = string
@@ -163,10 +60,6 @@ variable "coredns_addon_version" {
   type    = string
 }
 
-# variable "eks-ami" {
-#   default = ""
-# }
-
 variable "cerella_version" {
   default = "1.0.41"
 }
@@ -175,22 +68,9 @@ variable "deploy_cerella" {
   default = false
 }
 
-# variable "ingest_node_desired_capacity" {
-#   type    = number
-#   default = 0
-# }
-
-# variable "ingest-instance-type" {
-#   type = string
-# }
-
 variable "enable_eks_addons" {
   default = true
 }
-
-# variable "enable_ingest" {
-#   default = true
-# }
 
 variable "elasticsearch_override_file_name" {
   # if empty, then helm release will not use file to override default values
@@ -209,19 +89,3 @@ variable "cerella_green_override_file_name" {
   type    = string
   default = ""
 }
-
-# variable "intellegens_intermediate_licence" {
-#   default = ""
-# }
-
-# variable "intellegens_licence" {
-#   default = ""
-# }
-
-# variable "cdd_token" {
-#   default = ""
-# }
-
-# variable "create_secretsmanager" {
-#   default = false
-# }
