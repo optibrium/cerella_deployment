606cabe2fbf52e72585fe238e749a3ea6d04d32c
Shashank Parab <shashank@optibrium.com>
2023-02-07 13:00:54 +0000

    Example file

--- deployment/example.tf
+++ deployment/example.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "optibrium-devops"
-    key     = "terraform/sandbox_deployment.cerella.ai.tfstate"
-    profile = "root"
-    region  = "eu-west-2"
+    bucket  = "<bucket_name>"
+    key     = "<statefile>"
+    profile = "<AWS Profile>"
+    region  = "<AWS Region>"
   }
 }
 
@@ -11,16 +11,16 @@ data "terraform_remote_state" "cerella_infra" {
   backend = "s3"
 
   config = {
-    bucket  = "optibrium-devops"
-    key     = "terraform/sandbox.cerella.ai.tfstate"
-    profile = "root"
-    region  = "eu-west-2"
+    bucket  = "<bucket_name>"
+    key     = "<Infra statefile name>"
+    profile = "<AWS Profile>"
+    region  = "<AWS Region>"
   }
 }
 
 provider "aws" {
-  profile = "sandbox"
-  region  = "eu-west-1"
+  profile = "<AWS Profile>"
+  region  = "<AWS Region>"
 }
 
 module "cerella" {
@@ -28,14 +28,14 @@ module "cerella" {
   source                       = "../cerella"
   registry_username            = var.registry_username
   registry_password            = var.registry_password
-  region                       = "eu-west-1"
+  region                       = "<AWS Region>"
   cluster_name                 = data.terraform_remote_state.cerella_infra.outputs.eks_cluster_name
-  domain                       = "sandbox.cerella.ai"
+  domain                       = "<Domain>"
   deploy_cerella               = "true"
   external_secret_iam_role_arn = data.terraform_remote_state.cerella_infra.outputs.external_secret_iam_role_arn
   worker_nodes_iam_role_arn    = data.terraform_remote_state.cerella_infra.outputs.worker_nodes_iam_role_arn
   ingest_irsa_iam_role_name    = data.terraform_remote_state.cerella_infra.outputs.ingest_irsa_iam_role_name
   ingest_user_name             = data.terraform_remote_state.cerella_infra.outputs.ingest_user_name
   ingest_user_password         = data.terraform_remote_state.cerella_infra.outputs.ingest_user_password
-  cerella_version = "1.0.51"
+  cerella_version              = "1.0.51"
 }
